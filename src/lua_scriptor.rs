d075e1c059f58918c4f1444ac4378d56035e380e
connorjerow <cgjerow@gmail.com>
2025-07-07 16:15:10 -0700

    update errors

--- src/lua_scriptor.rs
+++ src/lua_scriptor.rs
@@ -12,21 +12,22 @@ impl LuaScriptor {
     }
 
     pub fn execute(&mut self, script: &str) -> LuaTable {
+        // by convention all lua scripts will be in src/scripts/
         let lua_code = fs::read_to_string(format!("src/scripts/{}.lua", script))
             .expect(&format!("Failed to read {}.lua", script));
         self.lua
             .load(&lua_code)
             .exec()
-            .expect(&format!("Failed to load script: {}", script));
+            .expect(&format!("Failed to load script: {}.lua", script));
 
         let globals = self.lua.globals();
         let main_fn: LuaFunction = globals.get("main").expect(&format!(
-            "Function `main` not defined for script: {}",
+            "Function `main` not defined for script: {}.lua",
             script
         ));
 
         return main_fn
             .call(())
-            .expect(&format!("Failed to execute script: {}", script));
+            .expect(&format!("Failed to execute script: {}.lua", script));
     }
 }
