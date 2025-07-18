b221e0519ca765388980842ae28884a5db4487bf
connorjerow <cgjerow@gmail.com>
2025-07-17 22:26:26 -0700

    maybe

--- src/scripts/systems/physics.lua
+++ src/scripts/systems/physics.lua
@@ -1,17 +1,37 @@
+local total = {
+	frame = 0,
+	decel = 0,
+}
+
 local function apply_drag_to_rigids(dt)
 	for id, elem in pairs(CONFIG.entities) do
 		if elem.type == GLOBALS.PHYSICS_BODIES.Rigid then
 			local vel = engine.get_velocity_2d(id) -- { vx, vy }
 
-			local decel = CONFIG.friction * dt
-			if decel < CONFIG.min_friction then
-				decel = CONFIG.min_friction
+			local drag = CONFIG.friction * dt
+			if drag < CONFIG.min_friction * dt then
+				drag = CONFIG.min_friction * dt
 			end
+
+			local speed = math.sqrt(vel[1] ^ 2 + vel[2] ^ 2)
+			if total.frame > 1 and id == WORLD.player_id() then
+				print("decel ", total.decel)
+				total.frame = 0
+				total.decel = 0
+			elseif id == WORLD.player_id() then
+				total.frame = total.frame + dt
+				total.decel = total.decel + drag
+			end
+
+			if id == WORLD.player_id() and speed > 40 then
+				print("v before", vel[1], vel[2])
+			end
+
 
 			-- Damping per component, clamped to avoid crossing zero
 			for i = 1, 2 do
 				local v = vel[i]
-				local damped = v * (1 - decel)
+				local damped = v * math.exp(-drag * dt)
 				-- If it would flip sign, clamp to 0 instead
 				if v > 0 and damped < 0 then
 					vel[i] = 0
@@ -23,7 +43,10 @@ local function apply_drag_to_rigids(dt)
 			end
 
 			-- 2) Enforce a max speed if not in a special state (e.g. dashing)
-			local speed = math.sqrt(vel[1] ^ 2 + vel[2] ^ 2)
+			speed = math.sqrt(vel[1] ^ 2 + vel[2] ^ 2)
+			if id == WORLD.player_id() and speed > 40 then
+				print("v vafter", vel[1], vel[2])
+			end
 			local max
 			if id == WORLD.player_id() then max = CONFIG.max_speed else max = CONFIG.skelly_speed end
 			if speed > max then
