424a7bb5e362f1d7a21a85db07d3750d787885c2
connorjerow <cgjerow@gmail.com>
2025-07-20 11:21:35 -0700

    create dashing skelly

--- src/scripts/characters/skelly.lua
+++ src/scripts/characters/skelly.lua
@@ -20,51 +20,71 @@ end
 local function move_skellies(dt)
 	local player_p = engine.get_position_2d(WORLD.player_id())
 	for key, value in pairs(CONFIG.entities) do
-		if value.is_skelly then
-			if WORLD.activity_state[key] and WORLD.activity_state[key].activity == "lunge" then
-				WORLD.activity_state[key].time = WORLD.activity_state[key].time - dt
-				if WORLD.activity_state[key].time <= 0 then
-					WORLD.activity_state[key].activity = "pursuing"
-					print("clear", WORLD.activity_state[key].activity)
-				end
+		if not value.is_skelly then goto continue end
+
+		local skelly_p = engine.get_position_2d(key)
+		local ex, ey = skelly_p[1], skelly_p[2]
+		local px, py = player_p[1], player_p[2]
+		-- Direction vector from enemy to player
+		local dx = px - ex
+		local dy = py - ey
+
+		ENGINE_HANDLES.flip_x(key, dx)
+
+		-- Length (magnitude) of the direction vector
+		local dist = math.sqrt(dx * dx + dy * dy)
+		if dist < 0.001 then
+			return
+		end
+
+		-- Normalize direction
+		local nx = dx / dist
+		local ny = dy / dist
+
+		if WORLD.activity_state[key] and WORLD.activity_state[key].activity == "lunge" then
+			WORLD.activity_state[key].time = WORLD.activity_state[key].time - dt
+			engine.set_velocity_2d(key, WORLD.activity_state[key].direction_x, WORLD.activity_state[key].direction_y)
+			if WORLD.activity_state[key].time <= 0 then
+				WORLD.activity_state[key].activity = "pursuing"
+				engine.set_velocity_2d(key, 0, 0)
+				print("clear", WORLD.activity_state[key].activity)
 			end
+			goto continue
+		end
 
-			local random_action = math.random(1, 12)
-			local skelly_p = engine.get_position_2d(key)
-			local ex, ey = skelly_p[1], skelly_p[2]
-			local px, py = player_p[1], player_p[2]
-			-- Direction vector from enemy to player
-			local dx = px - ex
-			local dy = py - ey
+		if WORLD.activity_state[key] and WORLD.activity_state[key].activity == "lunge-ramping" then
+			WORLD.activity_state[key].time = WORLD.activity_state[key].time - dt
+			if WORLD.activity_state[key].time <= 0 then
+				local fx = nx * 50
+				local fy = ny * 50
+				WORLD.activity_state[key].activity = "lunge"
+				WORLD.activity_state[key].time = .3
+				WORLD.activity_state[key].direction_x = fx
+				WORLD.activity_state[key].direction_y = fy
 
-			ENGINE_HANDLES.flip_x(key, dx)
-
-			-- Length (magnitude) of the direction vector
-			local dist = math.sqrt(dx * dx + dy * dy)
-			if dist < 0.001 then
-				return
-			end
-
-			-- Normalize direction
-			local nx = dx / dist
-			local ny = dy / dist
-
-			local not_lunging = (not WORLD.activity_state[key]) or (WORLD.activity_state[key].activity ~= "lunge")
-			local should_lunge = not_lunging and random_action < 9 and dist < 10 and
-					not ENGINE_HANDLES.is_untargetable(WORLD.player_id())
-
-
-			if should_lunge then
-				local fx = nx * 100
-				local fy = ny * 100
-				WORLD.activity_state[key] = { activity = "lunge", time = 2 }
 				engine.set_velocity_2d(key, fx, fy)
-			elseif random_action < 9 then
+
+				print("clear", WORLD.activity_state[key].activity)
+			end
+			goto continue
+		end
+
+
+		if (not WORLD.activity_state[key] or WORLD.activity_state[key].activity == "pursuing") then
+			local should_lunge = dist < 8
+			if should_lunge then
+				WORLD.activity_state[key] = { activity = "lunge-ramping", time = 2 }
+				engine.set_velocity_2d(key, 0, 0)
+			else
 				local fx = nx * 10
 				local fy = ny * 10
 				engine.set_velocity_2d(key, fx, fy)
 			end
+			goto continue
 		end
+
+
+		::continue::
 	end
 end
 
